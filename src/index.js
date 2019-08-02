578734e2106b31c71f23e74f714fd6266ba4e706
Santiago Marino Vasquez <samava1410@hotmail.com>
2019-08-01 22:24:42 -0500

    Configuraciones index.js y funcionamiento de rutas Inicial

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+//Conexion con la BD Mongo
+mongoose.connect('mongodb://localhost/Museo-Geologia', { useNewUrlParser: true })
+    .then(db => console.log('Conectado a la BD Museo-Geologia'))
+    .catch(err => console.log('Problemas de conexion de bases de datos' + err));
+
+mongoose.set('useCreateIndex', true);
+
+//Configuracion de las vistas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Configuracion de las rutas
+const routes = require('./routes/routes');
+app.use('/', routes);
+
+//Otras configuraciones
+app.use(express.urlencoded({extended: false}));
+
+//Iniciar servidor Express
+app.listen(3000, () => {
+    console.log('Servidor escuchando en :3000');
+});
